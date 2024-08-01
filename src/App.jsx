9548195c7c12a42f09e714648c2bd782c7bad395
samsur4D <patarehan14600@gmail.com>
2024-08-01 11:35:11 +0600

    uff

--- src/App.jsx
+++ src/App.jsx
@@ -29,13 +29,7 @@ import two from "../src/assets/obese.webp";
 import three from "../src/assets/slim.webp";
 import four from "../src/assets/medium-shot-man-working-gym-1-scaled.webp";
 import red from "../src/assets/Black and Red Gyms Back to Business Portrait Banner.png";
-// import bradly from "../src/assets/bradley-dunn-fjpl1yrNvNQ-unsplash.jpg";
 
-// import trainer1 from "../src/assets/smartfit-onelani-profile-new.jpg";
-// import trainer2 from "../src/assets/1702402108099.jfif";
-// import trainer3 from "../src/assets/yukgmll7jcsdiu0sufij.jpg";
-// import trainer4 from "../src/assets/gym-workout-personal-trainer-with-checklist-clipboard-consulting-training-sports-gym-black-man-muscular-active-smiling-fitness-coach-writing-health-wellness-exercise_590464-96995.avif";
-// import trainer5 from "../src/assets/Different-Types-of-Personal-Fitness-Trainers-And-Their-Average-Costs_--1024x538.webp";
 
 
 function App() {
@@ -139,13 +133,18 @@ function App() {
       {/* ########################### */}
 
 
-<section>
-  <div>
-    <h1 className="text-9xl text-white font-bold">About Us</h1>
+<section className="container mx-auto  mt-36  py-12 mb-36 w-full">
+  <div className="text-white">
+    <h1 className="text-9xl text-white text-center font-bold">About Us</h1>
 
-    <p>At Fitness Planet, we believe that fitness is a journey, not a destination. Our mission is to provide a welcoming and inclusive environment where everyone can achieve their fitness goals. Whether you're a seasoned athlete or just starting out, our team of dedicated professionals is here to support you every step of the way.</p>
+    <p className="text-justify mt-16 leading-tight text-4xl font-bold">At Fitness Planet, we believe that fitness is a journey, not a destination. Our mission is to provide a welcoming and inclusive environment where everyone can achieve their fitness goals. Whether you're a seasoned athlete or just starting out, our team of dedicated professionals is here to support you every step of the way.We aim to encourage and facilitate a healthy lifestyle for all our members. Our programs are designed to improve physical health, mental well-being, and overall quality of life.Fitness Planet is more than just a gym; it's a community. We strive to create a supportive and motivating environment where members can connect, share experiences, and inspire each other.We are committed to staying at the forefront of the fitness industry by continuously updating our equipment, facilities, and training techniques. Our goal is to provide the most effective and enjoyable fitness experience possible.Our evidence-based approach ensures that every member can achieve tangible results, whether it’s losing weight, building muscle, or improving overall health.From personalized training to group classes, our dedicated staff is here to support you every step of the way, ensuring you reach your fitness goals.Join us at Fitness Planet and take the first step towards a healthier, happier you. Our community is excited to welcome you and support you on your fitness journey!</p>
+
+   
   </div>
+ 
 </section>
+
+
 
 
 
